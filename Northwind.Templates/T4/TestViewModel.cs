0936ba712746486fe1323c8de9c32133bb68f1c2
Helena Coppieters <hcoppieters@hotmail.com>
2025-12-15 21:44:53 +0100

    T4 templates tests for ViewModels

---
+++ Northwind.Templates/T4/TestViewModel.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Shared.Models;
+#region EmployeeViewModel
+
+    public class EmployeeViewModel
+    {
+                    public int Id {get;set;}
+                    public string Name {get;set;}
+            }
+#endregion EmployeeViewModel
+
+ 
